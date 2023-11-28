3e5ce93b46e2397277d20ff0f7b94b7063d615bf
Muhammed ali TÜRK <103900615+muhammedaliturk@users.noreply.github.com>
2023-11-29 00:39:08 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+#include <cstdlib> 
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+    const int sizeofArray = 10;
+    int array[sizeofArray];
+    
+    for (int i = 0; i < sizeofArray; i++) {
+        cout << "please enter "<<i+1<<". element of array as integer " << endl;
+        cin >> array[i];
+    }
+   
+    for (int i = 0; i < sizeofArray; i++) {
+        int frequency = 0;
+        for (int j = 0; j < sizeofArray; j++) {
+            if (array[j] == array[i]) {
+                frequency++;
+            }
+        }
+        cout << array[i] << " used " << frequency << " times" << endl;
+    }
+    return 0;
+}
+
+
+
+
